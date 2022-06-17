ba3058b3dca89939559e85c1c0bbe87f7b317abf
goyoun <104054791+KoYounSung@users.noreply.github.com>
2022-06-17 16:15:35 +0900

    study

---
+++ src/main/java/com/example/restaurant/wishList/repository/WishListRepository.java
@@ -0,0 +1,13 @@
+package com.example.restaurant.wishList.repository;
+
+import com.example.restaurant.db.MemoryDbRepositoryAbstract;
+import com.example.restaurant.wishList.entity.WishListEntity;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class WishListRepository extends MemoryDbRepositoryAbstract<WishListEntity> {
+
+
+
+
+}
